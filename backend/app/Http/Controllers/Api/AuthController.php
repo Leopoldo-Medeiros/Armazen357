a9c26139554f01bbbaea3cb9ffe346edf45fb111
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2025-09-08 01:32:48 +0100

    User Registration API

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -3,21 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
+
+    /*
+     * unique:users -> garante que o email seja unico
+     * confirmed -> garante que a senha seja confirmada
+     * */
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
         $validate = $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required"string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', Password::min(8)],
+            'password_confirmation' => 'required|string|min:8|same:password',
         ]);
 
         $user = User::create([
-            'name' =>$validate['name'],
+            'name' => $validate['name'],
             'email' => $validate['email'],
-            'password' => $validate['password'],
+            'password' => Hash::make($validate['password']),
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
@@ -27,7 +37,6 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'user' => $user
         ], 201);
-
     }
     /**
      * Display a listing of the resource.
